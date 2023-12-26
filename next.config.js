896668dbb6e288055285553d67a9963760dcff8b
MuhammadSami223 <136311069+MuhammadSami223@users.noreply.github.com>
2023-12-26 16:09:06 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify:false
+  swcMinify:false,
+git config core.ignorecase false
 }
 
 module.exports = nextConfig
